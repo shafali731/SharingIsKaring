04e54be9405577d525b049e25e8a91bf0d3778a0
Karen Li <kli19@stuy.edu>
2018-12-07 03:27:42 -0500

    reading keys from json

--- util/movies.py
+++ util/movies.py
@@ -1,7 +1,13 @@
-import urllib.request, json
+import urllib.request, json, os
 
 #OMDb apikey=1891fe35
 #tastedive 324992-SoftdevP-U4MUASLY
+
+def get_key(key_type):
+    with open('keys.JSON', 'r') as f:
+        keys = json.load(f)
+        print(keys)
+    return keys.get(key_type)
 
 def remove_nonascii(text):
     '''removes non_ascii characters'''
@@ -12,8 +18,9 @@ def remove_nonascii(text):
 def movie_info(parameter, value):
     '''retrieves movie information from OMDb API based on the parameter and value'''
     try:
+ 
         url_stub = "http://www.omdbapi.com/?"
-        api_key = "apikey=1891fe35"
+        api_key = get_key('OMDB')
         search = parameter + remove_nonascii(value)
         url = url_stub + api_key + search
 
@@ -47,7 +54,7 @@ def better_movie_list(old_list):
 def movie_rec(parameter, value):
     '''retrieves movie recommendations based on the parameter and value'''
     url_stub = "https://tastedive.com/api/similar?"
-    api_key = "k=324992-SoftdevP-U4MUASLY"
+    api_key = get_key('tastedive_movie')
     search = parameter + value
     url = url_stub + api_key + search
     hdr = { 'User-Agent' : 'Mozilla/5.0' }
@@ -85,3 +92,4 @@ def name_from_id(id):
     '''Retrieves movie name based on id'''
     data = movie_info("&i=", id)
     return data.get("Title")
+
